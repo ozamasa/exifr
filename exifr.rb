fe0f30fe3379d2f0b5b9ed19f353b2f0a49b0e79
Masaki Ozawa <ozamasa123@gmail.com>
2016-10-09 11:42:32 +0900

    rubocop

--- exifr.rb
+++ exifr.rb
@@ -1,25 +1,25 @@
-require 'rubygems'
-require 'exifr'
-require 'find'
+require "rubygems"
+require "exifr"
+require "find"
 
-input  = '/Users/m_ozawa/Documents/ozawa/input'
-output = '/Users/m_ozawa/Documents/ozawa/output'
+input  = "./input"
+output = "./output"
 
 Find.find(input) do |f|
 	next unless FileTest.file?(f)
 	begin
-#if File.basename(f).gsub('.jpg', '')[-1].to_i > 1
+#if File.basename(f).gsub(".jpg", "")[-1].to_i > 1
 		tm = nil
 		fx = nil
-		if File.basename(f).downcase.index('.jpg')
+		if File.basename(f).downcase.index(".jpg")
 			if ex = EXIFR::JPEG.new(f)
 				tm = ex.date_time_original.strftime("%Y%m%d%H%M%S") unless ex.date_time_original.nil?
 				tm = File.mtime(f).strftime("%Y%m%d%H%M%S") if ex.date_time_original.nil?
-				fx = 'jpg'
+				fx = "jpg"
 			end
-		elsif File.basename(f).downcase.index('.mp4')
+		elsif File.basename(f).downcase.index(".mp4")
 			tm = File.mtime(f).strftime("%Y%m%d%H%M%S")
-			fx = 'mp4'
+			fx = "mp4"
 		end
 		if tm
 			cn = 0
